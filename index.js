0aa2ec84987ae863f7c9bfcd1bba1a16d659e93a
Chyld Medford <chyld.medford@gmail.com>
2015-06-14 02:12:24 -0700

    server

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Hoek = require('hoek');
+var Server = require('./lib/server');
+
+Server.init(function(err, server){
+  Hoek.assert(!err, err);
+  console.log('--------------------------------------------------------------------------------');
+  console.log('Hapi:\n', server.info.uri);
+  console.log('--------------------------------------------------------------------------------');
+});
