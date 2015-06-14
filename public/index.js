48ef48b69c7599e67e4216bd902680f0f52885d3
Chyld Medford <chyld.medford@gmail.com>
2015-06-14 14:14:10 -0700

    final

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
 angular.module('shipping', [])
 .controller('MainCtrl', function($scope, $http){
   $scope.create = function(label){
-    $http.post('/label', label);
+    $http.post('/label', label)
+    .then(function(response){
+      $scope.label = response.data.label;
+    });
   };
 });
